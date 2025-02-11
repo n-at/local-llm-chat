0403bacba85b57cd4ea227c7269b550dfdcdb708
Alexey Nurgaliev <atnurgaliev@gmail.com>
2025-02-11 19:47:34 +0300

    basic inference

--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 ```bash
 mkdir -m 0777 logs src/models
 
-wget -O "src/models/QVikhr-2.5-1.5B-Instruct-SMPO-Q3_K_M.gguf" "https://huggingface.co/Vikhrmodels/QVikhr-2.5-1.5B-Instruct-SMPO_GGUF/resolve/main/QVikhr-2.5-1.5B-Instruct-SMPO-Q3_K_M.gguf?download=true"
+wget -O "src/models/QVikhr-2.5-1.5B-Instruct-SMPO-Q4_K_M.gguf" "https://huggingface.co/Vikhrmodels/QVikhr-2.5-1.5B-Instruct-SMPO_GGUF/resolve/main/QVikhr-2.5-1.5B-Instruct-SMPO-Q4_K_M.gguf?download=true"
 
 cd src && npm install && cd ..
 
 docker compose up -d
 ```
+
+## Uses
+
+* [Vikhrmodels/QVikhr-2.5-1.5B-Instruct-SMPO](https://huggingface.co/Vikhrmodels/QVikhr-2.5-1.5B-Instruct-SMPO_GGUF) - Apache-2.0
+* [twbs/bootstrap](https://github.com/twbs/bootstrap) - MIT
+* [twbs/icons](https://github.com/twbs/icons) - MIT
+* [ngxson/wllama](https://github.com/ngxson/wllama) - MIT
+* [markedjs/marked](https://github.com/markedjs/marked) - MIT
--- src/main.js
+++ src/main.js
@@ -1,2 +1,65 @@
-import { Wllama } from './node_modules/@wllama/wllama/esm/index.js';
+import { Wllama, LoggerWithoutDebug } from './node_modules/@wllama/wllama/esm/index.js';
 
+const models = {
+    'QVikhr-2.5-1.5B': {
+        file: 'QVikhr-2.5-1.5B-Instruct-SMPO-Q4_K_M.gguf',
+        quant: 'Q4_K_M',
+    },
+};
+
+const languages = {
+    ru: {
+        name: 'Russian',
+    },
+    en: {
+        name: 'English',
+    },
+};
+
+function modelUrl(modelId) {
+    if (!models[modelId]) {
+        return null;
+    }
+    return `${location.origin}/models/${models[modelId].file}`;
+}
+
+const currentModelId = 'QVikhr-2.5-1.5B';
+const currentSystemPrompt = 'You are helpful digital assistant.';
+
+///////////////////////////////////////////////////////////////////////////////
+
+(async () => {
+    const CONFIG_PATHS = {
+        'single-thread/wllama.wasm': './node_modules/@wllama/wllama/esm/single-thread/wllama.wasm',
+        'multi-thread/wllama.wasm' : './node_modules/@wllama/wllama/esm/multi-thread/wllama.wasm',
+    };
+
+    const wllama = new Wllama(CONFIG_PATHS);
+
+    const progressCallback = ({loaded, total}) => {
+        const progressPercentage = Math.round((loaded / total) * 100);
+        console.log(`Downloading... ${progressPercentage}%`);
+    };
+
+    await wllama.loadModelFromUrl(modelUrl(currentModelId), {
+        progressCallback,
+        logger: LoggerWithoutDebug,
+    });
+
+    const messages = [
+        {role: 'system', content: currentSystemPrompt},
+        {role: 'user', content: 'Почему трава зеленая?'},
+    ];
+
+    console.log('THINKING...');
+
+    const output = await wllama.createChatCompletion(messages, {
+        sampling: {
+            temp: 0.5,
+            top_k: 40,
+            top_p: 0.9,
+        },
+    });
+
+    console.log(`OUTPUT: ${output}`);
+})();
